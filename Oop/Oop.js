c9890d35180fa7e2fc753772aca65affec2a7fce
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-02-22 17:57:44 +0600

    implement protypical inheritence in javascript function

--- Oop/Oop.js
+++ Oop/Oop.js
@@ -21,3 +21,61 @@ Student.prototype.intorduce = function() {
 const mike = new Student("mike", 2010, "CSE");
 mike.intorduce();
 mike.calcAge()
+
+
+
+
+// ///////////////////////////////////////
+// // Coding Challenge #3
+
+// /* 
+// 1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+// 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+// 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+// 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+// DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+// GOOD LUCK 😀
+
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+Car.prototype.break = function() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+}
+
+const bmw = new Car("BMW", 120);
+// bmw.accelerate();
+// bmw.break();
+// bmw.accelerate();
+
+
+
+const EV = function(make, speed, betteryCharge) {
+    Car.call(this, make, speed);
+    this.betteryCharge = betteryCharge;
+}
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo) {
+    this.betteryCharge = chargeTo;
+    console.log(`Tesla going at ${this.speed} km/h, with a charge of 22%`)
+}
+
+EV.prototype.accelerate = function() {
+    this.speed += 20;
+    this.betteryCharge--;
+    console.log(`Tesla going at ${this.speed} km/h, with a charge of 22%`)
+}
+
+const tesla = new EV("Tesla", 120, 23)
+
+tesla.accelerate()
