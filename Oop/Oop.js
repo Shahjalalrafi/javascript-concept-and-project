ad75646f9738913d50e8b2929a07928cac47ced6
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-03-12 21:16:36 +0600

    creating new custom method date object name lastYear

--- Oop/Oop.js
+++ Oop/Oop.js
@@ -295,8 +295,30 @@ let men = Object.create(obj1);
 men.age = 32;
 
 
-men.hello()
-console.log(men.age)
+// men.hello()
+// console.log(men.age)
 
 // console.log(obj1.__proto__)
 // console.log(obj2.hasOwnProperty("name"))
+
+
+
+
+
+//Exercise - extend the functionality of a built in object
+
+//#1
+//Date object => to have new method .lastYear() which shows you last year 'YYYY' format.
+
+Date.prototype.lastYear = function() {
+    return this.getFullYear() -1
+}
+
+console.log(new Date('1900-10-10').lastYear())
+//'1899'
+
+
+//#Bonus
+// Mofify .map() to print '🗺' at the end of each item.
+// console.log([1,2,3].map())
+//1🗺, 2🗺, 3🗺
